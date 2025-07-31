2f2fe698e30dd131f2465fe9a8a5d5ea77d6ada1
Jordan <cooliganpangjordan@gmail.com>
2025-07-31 17:22:09 -0400

    added models.py for data class access, developing deduplicator.py some more

--- src/deduplicator.py
+++ src/deduplicator.py
@@ -1,3 +1,42 @@
+import msg_parser
+import os
+from models import MessageInfo
+from typing import List, Dict
 
-def find_exact_file_duplicates(filepath):
-    return
+def collect_all_msg_info(directory_path: str) -> List[MessageInfo]:
+
+    all_messages = []
+
+    for filename in os.listdir(directory_path): 
+        root, extension = os.path.splitext(filename)
+
+        if filename.endswith(".msg"): 
+            file_path = os.path.join(directory_path, filename)
+            all_messages.append(msg_parser.extract_msg_info(file_path))
+
+    return all_messages
+
+def find_exact_file_duplicates(all_messages: List[MessageInfo]):
+    
+    dict_exact_duplicates = dict()
+
+    #iterate through each exisiting file in the given directory
+    for Message in all_messages:
+        
+        dict_exact_duplicates.setdefault(Message.file_hash, []).append(Message.filepath)
+        print(Message.filepath)
+
+    return dict_exact_duplicates
+        
+def find_thread_duplicates(all_messages: List[MessageInfo]):
+
+    
+
+    return None
+
+#test code to run only if initating in this module
+if __name__ == "__main__":
+    
+    foo = collect_all_msg_info("/home/jordan/msg_deduplicator/data")
+    
+    print(find_exact_file_duplicates(foo))
---
+++ src/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from dataclasses import dataclass
+from typing import Optional, List
+
+@dataclass
+class MessageInfo:
+    filepath: str
+    file_hash: str
+    subject: str
+    sender: str
+    date: Optional[datetime]
+    body_hash: str
+    message_id: Optional[str]
+    in_reply_to: Optional[str]
+    references: Optional[List[str]]
+
+    def print_data(self):
+        print("---------------------------")
+        print(f"File path: {self.filepath}")
+        print(f"File hash: {self.file_hash}")
+        print(f"Subject: {self.subject}")
+        print(f"Sender: {self.sender}")
+        print(f"Date: {self.date.strftime('%Y-%m-%d %H:%M:%S') if self.date else 'N/A'}")
+        print(f"Body_hash: {self.body_hash}")
+        print(f"Message_id: {self.message_id if self.message_id else 'N/A'}")
+        print(f"In reply to: {self.in_reply_to if self.in_reply_to else 'N/A'}")
+        print(f"References: {','.join(self.references) if self.references else 'N/A'}")
+        print("---------------------------")
